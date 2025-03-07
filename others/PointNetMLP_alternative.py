46223cd837c9bf595c30c702f9856bfe0b9f4873
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2025-03-07 02:38:41 -0800

    Update PointNetMLP_alternative.py

--- others/PointNetMLP_alternative.py
+++ others/PointNetMLP_alternative.py
@@ -241,10 +241,7 @@ class PointNetMLP(nn.Module):
 
         # Max pooling to get the global feature
         global_feature = F.max_pool1d(x, kernel_size=x.size(-1))
-        global_feature = global_feature.expand(-1, -1, num_points)
-
-        #global_feature = F.max_pool1d(x, kernel_size=x.size(-1))
-        #global_feature = global_feature.view(-1, global_feature.size(1), 1).expand(-1, -1, num_points)
+        global_feature = global_feature.view(-1, global_feature.size(1), 1).expand(-1, -1, num_points)
 
         # concatenate local and global features
         x = torch.cat([local_feature, global_feature], dim=1)
